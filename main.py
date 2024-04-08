b40672d85d6314b66c1a40a8c51f7eba3b5dd3c5
JesperVennstrom <jesper.vennstrom@elev.ga.ntig.se>
2024-04-08 14:36:03 +0200

    setup v2

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
 import pygame
 from config import *
 from sprites import *
+import sys 
+
+class Game: 
+    def __init__(self):
+        pygame.init()
+        pygame.displat.set_caption("Blancos Vs Niggers")
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font("freesansbold.ttf", 32)
+
+    def new(self):
+        self.playing = True
+
+    def main(self):
+        pass
+    
+    def generateTilemap(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def endScreen(self):
+        pass
+
+    def introScreen(self):
+        pass
+
+g = Game()
+g.introScreen()
+g.new()
+while g.running:
+    g.main()
+    g.endScreen()
+
+pygame.quit()
+sys.exit()
