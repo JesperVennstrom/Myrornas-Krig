ae207ac604c44e15abf8c86cb67b9cfb70b84baa
ArvidMarild <152278740+ArvidMarild@users.noreply.github.com>
2024-04-10 13:15:20 +0200

    setup-v3

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 import pygame
 from config import *
 from sprites import *
-import sys 
+import sys
 
 class Game: 
     def __init__(self):
         pygame.init()
-        pygame.displat.set_caption("Blancos Vs Niggers")
+        pygame.display.set_caption("Blancos Vs Niggers")
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font("freesansbold.ttf", 32)
+        self.running = True
 
     def new(self):
         self.playing = True
 
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.black = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates() #kanske inte behövs om vi har pvp
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 2)
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.quit:
+                self.playing = False
+                self.running = False
+
     def main(self):
-        pass
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
     
     def generateTilemap(self):
         pass
 
     def draw(self):
-        pass
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
 
     def update(self):
-        pass
+        self.all_sprites.update()
 
     def endScreen(self):
         pass
--- sprites.py
+++ sprites.py
@@ -1,2 +1,26 @@
 import pygame
 from config import *
+import math
+import random
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = PLAYER_LAYER
+        self.groups = self.game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+        self.width = TILESIZE
+        self.hight = TILESIZE
+
+        self.image = pygame.Surface([self.width, self.hight])
+        self.image.fill(RED)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        pass
